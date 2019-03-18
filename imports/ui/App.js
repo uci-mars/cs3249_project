d7d653c2ee7fe34961f31aced7068258907a00d2
Mars <32443134+uci-mars@users.noreply.github.com>
2019-03-18 20:23:55 +0800

    Reconfigured Meteor to use React as framework

---
+++ imports/ui/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// App component - represents the whole app
+export default class App extends Component {
+
+
+    render() {
+        return (
+          <div>
+            <h1>Hello World!</h1>
+            <p>(This is very ironic)</p>
+          </div>
+    );
+    }
+}
