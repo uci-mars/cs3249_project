e227cdb83a0114d36090a657de8760a9781b1315
Mars <32443134+uci-mars@users.noreply.github.com>
2019-04-15 21:32:59 +0800

    convert textfield to material ui picker

--- imports/api/temperature_data.js
+++ imports/api/temperature_data.js
@@ -2,3 +2,9 @@ import { Mongo } from 'meteor/mongo';
 
 export const temperature_data = new Mongo.Collection('temperature_data');
 
+if (Meteor.isServer) {
+    // This code only runs on the server
+    Meteor.publish('temperature_data', function tasksPublication() {
+        return temperature_data.find();
+    });
+}
--- imports/ui/Tools.jsx
+++ imports/ui/Tools.jsx
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Slider from '@material-ui/lab/Slider';
 import Typography from '@material-ui/core/Typography';
-
+import DateFnsUtils from "@date-io/date-fns";
+import { InlineDateTimePicker } from "material-ui-pickers";
 const toolStyle = {
     padding: "0px 0px 0px 30px",
     display: "inline-flex"
-}
+};
 
 const typoStyle = {
     padding: "10px"
@@ -20,27 +21,27 @@ const sliderStyles = {
 
 
 class Tools extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
 
-    state = {
-        value: 3,
+
+        };
+
+
     };
 
-    handleChangeInStartDate = name => event => {
-        this.props.updateDates([event.target.value, this.props.dates[1]]);
+    handleChangeInStartDate (date){
+        this.props.updateDates([date, this.props.dates[1]]);
     };
 
-    handleChangeInEndDate = name => event => {
-        this.props.updateDates([this.props.dates[0], event.target.value]);
+    handleChangeInEndDate (date) {
+        this.props.updateDates([this.props.dates[0], date]);
     };
 
     handleChange = (event, value) => {
         this.setState({ value });
     };
-
-    formatDate(date){
-        return date.toISOString();
-    }
-
 
     render(){
 
@@ -48,43 +49,79 @@ class Tools extends Component {
 
         return(
             <div style={toolStyle}>
-                <form noValidate autoComplete="on" >
-                    <TextField
-                        id="start"
-                        label="Start Date"
-                        type="datetime-local"
-                        defaultValue={this.props.dates[0]}
-                        onChange={this.handleChangeInStartDate('name')}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        margin="normal"
-                        variant="outlined"
+
+                <div className="picker">
+                    <InlineDateTimePicker
+                        keyboard
+                        minDate={new Date("2013-10-02T05:15:00")}
+                        maxDate={new Date("2013-12-03T15:30:00")}
+                        ampm={true}
+                        label="With keyboard"
+                        value={this.props.dates[0]}
+                        onChange={val => {this.handleChangeInStartDate(val)}}
+                        onError={console.log}
+                        format={'yyyy/MM/dd HH:mm A'}
+                        mask={[
+                            /\d/,
+                            /\d/,
+                            /\d/,
+                            /\d/,
+                            "/",
+                            /\d/,
+                            /\d/,
+                            "/",
+                            /\d/,
+                            /\d/,
+                            " ",
+                            /\d/,
+                            /\d/,
+                            ":",
+                            /\d/,
+                            /\d/,
+                        ]}
                     />
 
-                    <TextField
-                        id="end"
-                        label="End Date"
-                        type="datetime-local"
-                        defaultValue={this.props.dates[1]}
-                        onChange={this.handleChangeInEndDate('name')}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        margin="normal"
-                        variant="outlined"
+                    <InlineDateTimePicker
+                        keyboard
+                        minDate={new Date("2013-10-02T05:15:00")}
+                        maxDate={new Date("2013-12-03T15:30:00")}
+                        ampm={true}
+                        label="With keyboard"
+                        value={this.props.dates[1]}
+                        onChange={val => {this.handleChangeInEndDate(val)}}
+                        onError={console.log}
+                        format={'yyyy/MM/dd HH:mm A'}
+                        mask={[
+                            /\d/,
+                            /\d/,
+                            /\d/,
+                            /\d/,
+                            "/",
+                            /\d/,
+                            /\d/,
+                            "/",
+                            /\d/,
+                            /\d/,
+                            " ",
+                            /\d/,
+                            /\d/,
+                            ":",
+                            /\d/,
+                            /\d/,
+                        ]}
                     />
-                </form>
-                <div style={sliderStyles}>
-                    <Typography style={typoStyle}>No. of samples</Typography>
-                    <Slider
-                      value={value}
-                      min={0}
-                      max={6}
-                      step={1}
-                      onChange={this.handleChange}
-                    />
-                </div>   
+                </div>
+
+                {/*<div style={sliderStyles}>*/}
+                    {/*<Typography style={typoStyle}>No. of samples</Typography>*/}
+                    {/*<Slider*/}
+                      {/*value={value}*/}
+                      {/*min={0}*/}
+                      {/*max={6}*/}
+                      {/*step={1}*/}
+                      {/*onChange={this.handleChange}*/}
+                    {/*/>*/}
+                {/*</div>   */}
             </div>     
         );
 
